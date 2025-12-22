08d8974ac3e9df1ecd31edeb7dcaa1ef3606acea
Negin Ahmadi <negin.ahmadi.tech@gmail.com>
2025-12-22 12:07:37 +0330

    Javascript - Two Fer solved

---
+++ javascript/two-fer/two-fer.js
@@ -0,0 +1,15 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+/**
+ *
+ * @param {String | undefined} name
+ */
+export const twoFer = (name) => {
+  if (name) {
+    return `One for ${name}, one for me.`;
+  }
+
+  return "One for you, one for me.";
+};
