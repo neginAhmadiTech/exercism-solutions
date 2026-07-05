5d643ba804cd827ddeed3e8c36cb28f987e66d11
Negin Ahmadi <negin.ahmadi.tech@gmail.com>
2026-07-05 10:02:08 +0330

    Python - Reverse String solved

---
+++ python/reverse-string/reverse_string.py
@@ -0,0 +1,13 @@
+"""Module to reverse a given string
+"""
+def reverse(text):
+    """Function to reverse a given string
+
+    Args:
+        text (str): The string to be reversed
+
+    Returns:
+        str: The reversed string
+    """
+    text = text[::-1]
+    return text
