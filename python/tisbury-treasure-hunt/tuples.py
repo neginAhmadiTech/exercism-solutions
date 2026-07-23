c0de7b9ee430196279777bfe5eb1d77b22bd96ac
Negin Ahmadi <negin.ahmadi.tech@gmail.com>
2026-07-23 21:33:27 +0330

    Python - Tisbury Treasure Hunt solved

---
+++ python/tisbury-treasure-hunt/tuples.py
@@ -0,0 +1,89 @@
+"""Functions to help Azara and Rui locate pirate treasure."""
+
+
+def get_coordinate(record):
+    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
+
+    Parameters:
+        record (tuple): A (treasure, coordinate) pair.
+
+    Returns:
+        str: The extracted map coordinate.
+    """
+
+    return record[-1]
+
+
+def convert_coordinate(coordinate):
+    """Split the given coordinate into tuple containing its individual components.
+
+    Parameters:
+        coordinate (str): A string map coordinate.
+
+    Returns:
+        tuple: The string coordinate split into its individual components.
+    """
+
+    return (coordinate[0], coordinate[1])
+
+
+def compare_records(azara_record, rui_record):
+    """Compare two record types and determine if their coordinates match.
+
+    Parameters:
+        azara_record (tuple): A (treasure, coordinate) pair.
+        rui_record (tuple): A (location, tuple(coordinate_1, coordinate_2), quadrant) trio.
+
+    Returns:
+        bool: Do the coordinates match?
+    """
+
+    if azara_record[1] == rui_record[1][0] + rui_record[1][1]:
+        return True
+
+    return False
+
+
+def create_record(azara_record, rui_record):
+    """Combine the two record types (if possible) and create a combined record group.
+
+    Parameters:
+        azara_record (tuple): A (treasure, coordinate) pair.
+        rui_record (tuple): A (location, coordinate, quadrant) trio.
+
+    Returns:
+        tuple or str: The combined record (if compatible),
+        or the string "not a match" (if incompatible).
+    """
+
+    if azara_record[1] == rui_record[1][0] + rui_record[1][1]:
+        return azara_record + rui_record
+
+    return "not a match"
+
+
+def clean_up(combined_record_group):
+    """Clean up a combined record group into a multi-line string of single records.
+
+    Parameters:
+        combined_record_group (tuple): Everything from both participants.
+
+    Returns:
+        str: Everything "cleaned", excess coordinates and information are removed.
+
+    Note:
+        The return statement is a multi-lined string with items separated by newlines.
+        (see HINTS.md for an example).
+
+    """
+
+    result = ""
+    for item in combined_record_group:
+
+        item_list = list(item)
+        item_list.remove(item_list[1])
+        item = tuple(item_list)
+
+        result += f"{str(item)}\n"
+
+    return result
