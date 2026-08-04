f3627fe04b4d75828a2a7093c06ca9f53c8c94fd
Negin Ahmadi <negin.ahmadi.tech@gmail.com>
2026-08-05 01:25:34 +0330

    Python - Word Count solved

---
+++ python/word-count/word_count.py
@@ -0,0 +1,30 @@
+"""Module for counting the occurrences of each word in a sentence.
+"""
+def count_words(sentence):
+    """Function to count the occurrences of each word in a sentence.
+
+    Args:
+        sentence (str): The input sentence.
+
+    Returns:
+        dict: A dictionary with words as keys and their counts as values.
+    """
+
+    sentence = sentence.lower()
+    words = []
+
+    for letter in sentence:
+        if letter in "_.,:;!@#$%^&*()[]{}\"`~":
+            sentence = sentence.replace(letter, " ")
+
+    for word in sentence.split():
+
+        word = word.strip("'")
+
+        if word:
+            words.append(word)
+
+    word_count = {}
+    for word in words:
+        word_count.setdefault(word, words.count(word))
+    return word_count
