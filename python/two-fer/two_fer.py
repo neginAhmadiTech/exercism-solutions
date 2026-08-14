e9a08cd72ba8eb12f0a327dcb7b049c1e071b262
Negin Ahmadi <negin.ahmadi.tech@gmail.com>
2026-08-14 11:39:26 +0330

    Python - Two Fer solved

---
+++ python/two-fer/two_fer.py
@@ -0,0 +1,12 @@
+"""Module to tell a sentence base of the existance
+of the input - solution 2 -> using default value
+"""
+
+
+def two_fer(name="you"):
+    """Returns a sentence based on we have input or not
+
+    Returns:
+        str: The given sentence based on the input
+    """
+    return f"One for {name}, one for me."
