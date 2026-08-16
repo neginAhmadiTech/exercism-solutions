630f3e1a154a65e6bfad3f9f7e30888d6e222ad4
Negin Ahmadi <negin.ahmadi.tech@gmail.com>
2026-08-16 11:36:46 +0330

    Python - Say solved (iteration 2 - refactored)

--- python/say/say.py
+++ python/say/say.py
@@ -20,53 +20,57 @@ def handle_ones(number):
             return "eight"
         case 9:
             return "nine"
+        case _:
+            return ""
+
+
+def handle_tens_special_numbers(number):
+    match (number):
+        case 0:
+            return "ten"
+        case 1:
+            return "eleven"
+        case 2:
+            return "twelve"
+        case 3:
+            return "thirteen"
+        case 5:
+            return "fifteen"
+        case 8:
+            return "eighteen"
+        case _:
+            return f"{handle_ones(number)}teen"
 
 
 def handle_tens(number):
     remainder = number % 10
     divisor = number // 10
 
-    match (divisor):
-        case 1:
-            match (remainder):
-                case 0:
-                    return "ten"
-                case 1:
-                    return "eleven"
-                case 2:
-                    return "twelve"
-                case 3:
-                    return "thirteen"
-                case 5:
-                    return "fifteen"
-                case 8:
-                    return "eighteen"
-                case _:
-                    return f"{handle_ones(remainder)}teen"
-        case 2:
-            if remainder == 0:
-                return "twenty"
-            return f"twenty-{handle_ones(remainder)}"
-        case 3:
-            if remainder == 0:
-                return "thirty"
-            return f"thirty-{handle_ones(remainder)}"
-        case 5:
-            if remainder == 0:
-                return "fifty"
-            return f"fifty-{handle_ones(remainder)}"
-        case 8:
-            if remainder == 0:
-                return "eighty"
-            return f"eighty-{handle_ones(remainder)}"
-        case 4:
-            if remainder == 0:
-                return "forty"
-            return f"forty-{handle_ones(remainder)}"
-        case _:
-            if remainder == 0:
-                return f"{handle_ones(divisor)}ty"
-            return f"{handle_ones(divisor)}ty-{handle_ones(remainder)}"
+    tens = {
+        1: handle_tens_special_numbers(remainder),
+        2: "twenty" if remainder == 0 else f"twenty-{handle_ones(remainder)}",
+        3: "thirty" if remainder == 0 else f"thirty-{handle_ones(remainder)}",
+        4: "forty" if remainder == 0 else f"forty-{handle_ones(remainder)}",
+        5: "fifty" if remainder == 0 else f"fifty-{handle_ones(remainder)}",
+        6: (
+            f"{handle_ones(divisor)}ty"
+            if remainder == 0
+            else f"{handle_ones(divisor)}ty-{handle_ones(remainder)}"
+        ),
+        7: (
+            f"{handle_ones(divisor)}ty"
+            if remainder == 0
+            else f"{handle_ones(divisor)}ty-{handle_ones(remainder)}"
+        ),
+        8: "eighty" if remainder == 0 else f"eighty-{handle_ones(remainder)}",
+        9: (
+            f"{handle_ones(divisor)}ty"
+            if remainder == 0
+            else f"{handle_ones(divisor)}ty-{handle_ones(remainder)}"
+        ),
+    }
+
+    return tens[divisor]
 
 
 def handle_hundreds(number):
@@ -85,7 +89,7 @@ def say(number):
 
     number_str = str(number)
     separated_number = [
-        number_str[max(0, i - 3) : i] for i in range(len(number_str), 0, -3)
+        number_str[max(0, index - 3) : index] for index in range(len(number_str), 0, -3)
     ]
 
     result = ""
@@ -102,18 +106,28 @@ def say(number):
             result += f" thousand {handle_hundreds(int(separated_number[-2]))}"
 
         case 3:
-            thousand_number = handle_hundreds(int(separated_number[-2]))
-            thousand = "thousand" if handle_hundreds(int(separated_number[-2])) else ""
+            thousand_part = (
+                handle_hundreds(int(separated_number[-2])) + " thousand"
+                if handle_hundreds(int(separated_number[-2]))
+                else ""
+            )
+
             hundred_number = handle_hundreds(int(separated_number[-3]))
-            result += f" million {thousand_number} {thousand} {hundred_number}"
+            result += f" million {thousand_part} {hundred_number}"
 
         case 4:
-            million_number = handle_hundreds(int(separated_number[-2]))
-            million = "million" if handle_hundreds(int(separated_number[-2])) else ""
-            thousand_number = handle_hundreds(int(separated_number[-3]))
-            thousand = "thousand" if handle_hundreds(int(separated_number[-3])) else ""
-            hundred_number = handle_hundreds(int(separated_number[-4]))
+            million_part = (
+                handle_hundreds(int(separated_number[-2])) + " million"
+                if handle_hundreds(int(separated_number[-2]))
+                else ""
+            )
+            thousand_part = (
+                handle_hundreds(int(separated_number[-3])) + " thousand"
+                if handle_hundreds(int(separated_number[-3]))
+                else ""
+            )
+            hundred_part = handle_hundreds(int(separated_number[-4]))
 
-            result += f" billion {million_number} {million} {thousand_number} {thousand} {hundred_number}"
+            result += f" billion {million_part} {thousand_part} {hundred_part}"
 
     return result.strip()
