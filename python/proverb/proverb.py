17e868e59a92d9bdeaf4799a64e06d4355d8e68f
Negin Ahmadi <negin.ahmadi.tech@gmail.com>
2026-08-17 21:13:01 +0330

    refactor: Python - Proverb solved
    Improved the readability of the code + memory optimization

--- python/proverb/proverb.py
+++ python/proverb/proverb.py
@@ -1,21 +1,19 @@
 def proverb(*args, qualifier):
+
+    if not args:
+        return []
+
     result = []
-    first_card = ""
-
-    if len(args) == 0:
-        return result
-
-    if len(args) > 0:
-        first_card = args[0]
+    first_card = args[0]
 
     while len(args) > 1:
         first, *args = args
-        sentence = f"For want of a {first} the {args[0]} was lost."
+        second = args[0]
+        sentence = f"For want of a {first} the {second} was lost."
         result.append(sentence)
 
-    final_sentence = (
-        f"And all for the want of a {qualifier+" " if qualifier else ""}{first_card}."
-    )
+    qualified_card = f"{qualifier} " if qualifier else ""
+    final_sentence = f"And all for the want of a {qualified_card}{first_card}."
 
     result.append(final_sentence)
 
