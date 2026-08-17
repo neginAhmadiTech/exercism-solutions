904f16341c1e7c5e28d58a8199ce07d9d09760bd
Negin Ahmadi <negin.ahmadi.tech@gmail.com>
2026-08-17 17:51:09 +0330

    refactor: Python - Scrabble Score solved
    Reduced loops and complexity inside them

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,11 +1,30 @@
 LETTER_SCORES = {
-    1: "aeioulnrst",
-    2: "dg",
-    3: "bcmp",
-    4: "fhvwy",
-    5: "k",
-    8: "jx",
-    10: "qz",
+    "a": 1,
+    "e": 1,
+    "i": 1,
+    "o": 1,
+    "u": 1,
+    "l": 1,
+    "n": 1,
+    "r": 1,
+    "s": 1,
+    "t": 1,
+    "d": 2,
+    "g": 2,
+    "b": 3,
+    "c": 3,
+    "m": 3,
+    "p": 3,
+    "f": 4,
+    "h": 4,
+    "v": 4,
+    "w": 4,
+    "y": 4,
+    "k": 5,
+    "j": 8,
+    "x": 8,
+    "q": 10,
+    "z": 10,
 }
 
 
@@ -14,9 +33,6 @@ def score(word):
     result = 0
 
     for letter in word:
-        for point, letters in LETTER_SCORES.items():
-            if letter in letters:
-                result += point
-                break
+        result += LETTER_SCORES[letter]
 
     return result
