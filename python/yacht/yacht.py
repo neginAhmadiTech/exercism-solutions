07a691f11ab26113e2c5b9c04bd0477308c949aa
Negin Ahmadi <negin.ahmadi.tech@gmail.com>
2026-08-20 12:40:38 +0330

    refactor: Python - Yacht solved

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -12,16 +12,24 @@ BIG_STRAIGHT = 10
 CHOICE = 11
 
 
-def correct_frequency(dice, number_types, frequency):
+def correct_frequency(dice, frequency):
     dice_set = set(dice)
-    if len(dice_set) not in number_types:
+    if len(dice_set) != 2:
         return False
 
-    number_1_frequency = len([val for val in dice if val == list(dice_set)[0]])
-    if number_1_frequency not in frequency:
+    first_number_frequency = dice.count(list(dice_set)[0])
+    if first_number_frequency not in frequency:
         return False
 
     return True
+
+
+def score_yacht(dice):
+    for item in dice:
+        if dice.count(item) == 5:
+            return 50
+
+    return 0
 
 
 def score_big_straight(dice):
@@ -41,57 +49,47 @@ def score_little_straight(dice):
 
 
 def score_four_of_a_kind(dice):
-    if not correct_frequency(dice, [1, 2], [1, 4, 5]):
-        return 0
+    for item in dice:
+        if dice.count(item) == 4 or dice.count(item) == 5:
+            return 4 * item
 
-    dice_set = set(dice)
-    number_frequency = len([val for val in dice if val == list(dice_set)[0]])
-
-    if number_frequency == 1:
-        number_frequency = len([val for val in dice if val == list(dice_set)[1]])
-        return number_frequency * list(dice_set)[1]
-
-    return 4 * list(dice_set)[0]
+    return 0
 
 
 def score_full_house(dice):
 
-    if not correct_frequency(dice, [2], [2, 3]):
+    if not correct_frequency(dice, [2, 3]):
         return 0
 
     return sum(dice)
 
 
 def score_numbers(dice, number):
-    number_frequency = len([val for val in dice if val == number])
+    number_frequency = dice.count(number)
 
     return number * number_frequency
 
 
 def score(dice, category):
-    result = 0
 
     match (category):
         case value if value in (ONES, TWOS, THREES, FOURS, FIVES, SIXES):
-            result = score_numbers(dice, category)
+            return score_numbers(dice, category)
 
         case value if value == FULL_HOUSE:
-            result = score_full_house(dice)
+            return score_full_house(dice)
 
         case value if value == FOUR_OF_A_KIND:
-            result = score_four_of_a_kind(dice)
+            return score_four_of_a_kind(dice)
 
         case value if value == LITTLE_STRAIGHT:
-            result = score_little_straight(dice)
+            return score_little_straight(dice)
 
         case value if value == BIG_STRAIGHT:
-            result = score_big_straight(dice)
+            return score_big_straight(dice)
 
         case value if value == CHOICE:
-            result = sum(dice)
+            return sum(dice)
 
         case value if value == YACHT:
-            if correct_frequency(dice, [1], [5]):
-                result = 50
-
-    return result
+            return score_yacht(dice)
