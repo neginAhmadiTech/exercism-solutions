a9f393207de4fbbc05c6b190c4ebea988ef492ac
Negin Ahmadi <negin.ahmadi.tech@gmail.com>
2026-08-21 14:59:35 +0330

    refactor: Python - Grade School solved

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -3,34 +3,30 @@ from collections import defaultdict
 
 class School:
     def __init__(self):
-        self.school = defaultdict(list)
+        self.students_by_grade = defaultdict(list)
+        self.students = set()
         self.added_list = []
-
-    def _check_duplicate_name(self, name):
-        for students in self.school.values():
-            if name in students:
-                return True
-        return False
 
     def add_student(self, name, grade):
 
-        if self._check_duplicate_name(name):
+        if name in self.students:
             self.added_list.append(False)
             return
 
-        self.school[grade].append(name)
+        self.students_by_grade[grade].append(name)
+        self.students.add(name)
         self.added_list.append(True)
 
     def roster(self):
         result = []
-        for item in sorted(self.school.items()):
-            for student in sorted(item[1]):
+        for grade, students in sorted(self.students_by_grade.items()):
+            for student in sorted(students):
                 result.append(student)
 
         return result
 
     def grade(self, grade_number):
-        return sorted(self.school.get(grade_number, []))
+        return sorted(self.students_by_grade.get(grade_number, []))
 
     def added(self):
         return self.added_list
