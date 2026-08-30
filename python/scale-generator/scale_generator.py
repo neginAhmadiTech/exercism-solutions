103d555738d51d7515022637b8f91302e88d7405
Negin Ahmadi <negin.ahmadi.tech@gmail.com>
2026-08-30 09:20:18 +0330

    refactor: Python - Scale Generator solved

--- python/scale-generator/scale_generator.py
+++ python/scale-generator/scale_generator.py
@@ -1,21 +1,26 @@
 SHARP_NOTES = ["G#", "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G"]
 FLAT_NOTES = ["F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E"]
 
-FLATS = ["F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb"]
+TONICS_USING_FLATS = ["F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb"]
+INTERVAL_STEPS = {
+    "m": 1,
+    "M": 2,
+    "A": 3,
+}
 
 
 class Scale:
     def __init__(self, tonic):
         self.tonic = tonic
-        self.tonic_sanitized = tonic.replace(tonic[0], tonic[0].upper(), 1)
+        self.tonic_sanitized = tonic.capitalize()
 
     def chromatic(self):
 
         notes = []
-        if self.tonic in FLATS:
-            notes = FLAT_NOTES.copy()
+        if self.tonic in TONICS_USING_FLATS:
+            notes = FLAT_NOTES
         else:
-            notes = SHARP_NOTES.copy()
+            notes = SHARP_NOTES
 
         start_index = notes.index(self.tonic_sanitized)
 
@@ -27,21 +32,13 @@ class Scale:
         return result
 
     def interval(self, intervals):
+
         notes = self.chromatic()
+        current_index = 0
 
-        start_index = notes.index(self.tonic_sanitized)
-
-        result = []
-        result.append(self.tonic_sanitized)
+        result = [notes[current_index]]
         for interval in intervals:
-
-            if interval == "m":
-                start_index = (start_index + 1) % 12
-            elif interval == "M":
-                start_index = (start_index + 2) % 12
-            elif interval == "A":
-                start_index = (start_index + 3) % 12
-
-            result.append(notes[start_index])
+            current_index = (current_index + INTERVAL_STEPS[interval]) % 12
+            result.append(notes[current_index])
 
         return result
