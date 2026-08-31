165656587a71c2891a76e5f7bb0b40f88264a897
Negin Ahmadi <negin.ahmadi.tech@gmail.com>
2026-08-31 14:59:22 +0330

    refactor: Python - Markdown solved

--- python/markdown/markdown.py
+++ python/markdown/markdown.py
@@ -4,51 +4,45 @@ import re
 def make_headings(line):
 
     if re.match("###### (.*)", line):
+
         line = "<h6>" + line[7:] + "</h6>"
 
     elif re.match("##### (.*)", line):
+
         line = "<h5>" + line[6:] + "</h5>"
 
     elif re.match("#### (.*)", line):
+
         line = "<h4>" + line[5:] + "</h4>"
 
     elif re.match("### (.*)", line):
+
         line = "<h3>" + line[4:] + "</h3>"
 
     elif re.match("## (.*)", line):
+
         line = "<h2>" + line[3:] + "</h2>"
 
     elif re.match("# (.*)", line):
+
         line = "<h1>" + line[2:] + "</h1>"
 
     return line
 
 
-def make_bold(unordered_list):
-    return (
-        unordered_list.group(1)
-        + "<strong>"
-        + unordered_list.group(2)
-        + "</strong>"
-        + unordered_list.group(3)
-    )
+def make_bold(match):
+    return match.group(1) + "<strong>" + match.group(2) + "</strong>" + match.group(3)
 
 
-def make_italic(unordered_list):
-    return (
-        unordered_list.group(1)
-        + "<em>"
-        + unordered_list.group(2)
-        + "</em>"
-        + unordered_list.group(3)
-    )
+def make_italic(match):
+    return match.group(1) + "<em>" + match.group(2) + "</em>" + match.group(3)
 
 
-def is_bold(item):
+def find_bold_match(item):
     return re.match("(.*)__(.*)__(.*)", item)
 
 
-def is_italic(item):
+def find_italic_match(item):
     return re.match("(.*)_(.*)_(.*)", item)
 
 
@@ -71,12 +65,15 @@ def parse(markdown):
             current_item = unordered_list.group(1)
 
             # make html bold sign
-            if is_bold(current_item):
-                current_item = make_bold(is_bold(current_item))
+            bold_match = find_bold_match(current_item)
+
+            if bold_match:
+                current_item = make_bold(bold_match)
 
             # make html italic sign
-            if is_italic(current_item):
-                current_item = make_italic(is_italic(current_item))
+            italic_match = find_italic_match(current_item)
+            if italic_match:
+                current_item = make_italic(italic_match)
 
             # makes the first list item
             if not item_in_list:
@@ -95,11 +92,13 @@ def parse(markdown):
         if another_tag_open is None:
             line = "<p>" + line + "</p>"
 
-        if is_bold(line):
-            line = make_bold(is_bold(line))
+        bold_match = find_bold_match(line)
+        if bold_match:
+            line = make_bold(bold_match)
 
-        if is_italic(line):
-            line = make_italic(is_italic(line))
+        italic_match = find_italic_match(line)
+        if italic_match:
+            line = make_italic(italic_match)
 
         if item_added_to_list:
             # ends the list
@@ -111,10 +110,3 @@ def parse(markdown):
         result += "</ul>"
 
     return result
-
-
-# parse("* __Bold Item__\n* _Italic Item_")
-# parse("# Header!\n* __Bold Item__\n* _Italic Item_\n* __Bold Item__")
-# parse("# Start a list\n* Item 1\n* Item 2\nEnd a list")
-parse("# Start a list\n* Item 1\n* Item 2\nEnd a list")
-# "<h1>Start a list</h1><ul><li>Item 1</li><li>Item 2</li></ul><p>End a list</p>"
