42a92b730a53ed16bddb22a0837a95f8e81db12e
Negin Ahmadi <negin.ahmadi.tech@gmail.com>
2026-09-11 20:05:58 +0330

    refactor: Python - Bank Account solved (Lock threading added for parallel operations)

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -1,10 +1,14 @@
+import threading
+
+
 class BankAccount:
     def __init__(self):
         self.balance = 0
-        self.is_opened = False
+        self.is_open = False
+        self.lock = threading.Lock()
 
     def check_account_open(self):
-        if not self.is_opened:
+        if not self.is_open:
             raise ValueError("account not open")
 
     def check_negative_amount(self, amount):
@@ -13,37 +17,39 @@ class BankAccount:
 
     def get_balance(self):
 
-        self.check_account_open()
-
-        return self.balance
+        with self.lock:
+            self.check_account_open()
+            return self.balance
 
     def open(self):
+        with self.lock:
 
-        if self.is_opened:
-            raise ValueError("account already open")
+            if self.is_open:
+                raise ValueError("account already open")
 
-        self.is_opened = True
-        self.balance = 0
+            self.is_open = True
+            self.balance = 0
 
     def deposit(self, amount):
 
-        self.check_account_open()
-        self.check_negative_amount(amount)
+        with self.lock:
+            self.check_account_open()
+            self.check_negative_amount(amount)
 
-        self.balance += amount
+            self.balance += amount
 
     def withdraw(self, amount):
 
-        self.check_account_open()
-        self.check_negative_amount(amount)
+        with self.lock:
+            self.check_account_open()
+            self.check_negative_amount(amount)
 
-        if amount > self.balance:
-            raise ValueError("amount must be less than balance")
+            if amount > self.balance:
+                raise ValueError("amount must be less than balance")
 
-        self.balance -= amount
+            self.balance -= amount
 
     def close(self):
-
-        self.check_account_open()
-
-        self.is_opened = False
+        with self.lock:
+            self.check_account_open()
+            self.is_open = False
