5bde6c450fd5d6fd09884a68a663815258ef1dc1
Negin Ahmadi <negin.ahmadi.tech@gmail.com>
2026-09-20 09:58:27 +0330

    Python - Zipper solved

---
+++ python/zipper/zipper.py
@@ -0,0 +1,57 @@
+class Zipper:
+    def __init__(self, tree):
+        self.tree = tree
+        self.focus = tree
+        self.path = []
+
+    @staticmethod
+    def from_tree(tree):
+        return Zipper(tree)
+
+    def value(self):
+        return self.focus["value"]
+
+    def set_value(self, value):
+        self.focus["value"] = value
+        return self
+
+    def left(self):
+
+        if self.focus["left"] is None:
+            return None
+
+        self.path.append(self.focus)
+        self.focus = self.focus["left"]
+
+        return self
+
+    def set_left(self, left):
+
+        self.focus["left"] = left
+        return self
+
+    def right(self):
+
+        if self.focus["right"] is None:
+            return None
+
+        self.path.append(self.focus)
+        self.focus = self.focus["right"]
+        return self
+
+    def set_right(self, right):
+
+        self.focus["right"] = right
+        return self
+
+    def up(self):
+
+        if not self.path:
+            return None
+
+        self.focus = self.path.pop()
+        return self
+
+    def to_tree(self):
+        self.focus = None
+        return self.tree
